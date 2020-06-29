58d5b1418726c05896b9be6d0f5b9d61399fb483
frappelatte28 <64382399+frappelatte28@users.noreply.github.com>
2020-06-30 01:29:46 +0530

    added Gameover alert and fixed some minor bugs
    Removed lower wall and showed a alert of gameover
    made canvas responsive
    reloaded canvas on resizing

--- game.js
+++ game.js
@@ -8,31 +8,8 @@ canvas.height = height;
 
 canvas.style.background ="blue"
 
-
-
-var pallet_width = 90
-var pallet_height = 15
-var palletX = canvas.width/2
-var palletY = canvas.height-pallet_height
 var rightPressed = false
 var leftPressed = false
-
-
-
-var gupdate = function(){
-    
-    console.log(this)
-    //velocity upsate
-    this.x+= this.dx
-    this.y+= this.dy 
-
-    //boundayry pndn
-    
-    if((this.x  +this.dx)  > canvas.width - this.radius  || (this.x + this.dx) < this.radius)
-        this.dx = -this.dx
-    if((this.y + this.dy)>canvas.height - this.radius || (this.y + this.dy) < this.radius)
-        this.dy = -this.dy
-}
 
 var circle = {
     x: canvas.width/2,
@@ -40,7 +17,22 @@ var circle = {
     dx:2,
     dy:-2,
     radius:20,
-    update:gupdate,
+    update:function(){
+        //velocity update
+    this.x+= this.dx
+    this.y+= this.dy 
+    //boundary condition
+    if((this.x  +this.dx)  > canvas.width - this.radius  || (this.x + this.dx) < this.radius)
+        this.dx = -this.dx
+    if((this.y + this.dy) < this.radius){
+        this.dy = -this.dy
+    }
+    else if( this.y > canvas.height - this.radius){
+        alert("GAME OVER")
+        window.location.reload()
+        clearInterval(interval)
+    }
+    },
     draw:function(){
         context.beginPath()
         context.arc(this.x, this.y,  this.radius, 0, Math.PI*2)
@@ -49,12 +41,38 @@ var circle = {
     }
 }
 
+var pallet= {
+    x:canvas.width/2,
+    y:canvas.height-10,
+    width:90,
+    height:10,
+    update: function(){
+        if(rightPressed){
+            this.x+= 10
+            console.log("right")
+            if (this.x + this.width > canvas.width){
+                this.x = canvas.width - this.width
+            }
+        }
+        else if(leftPressed){
+        this.x+= -10
+        if(this.x < 0){
+        this.x = 0
+        }
+    }
+    },
+    draw:function(){
+        context.beginPath()
+        context.rect(this.x,this.y,this.width,this.height)
+        context.fillStyle = 'black'
+        context.fill()
+    }
+}
 
 window.addEventListener('keydown',keyDownHandler)
 window.addEventListener('keyup',keyUpHandler)
 
 function keyDownHandler(event){
-    console.log("keydown")
     if(event.key == 'Right'|| event.key == 'ArrowRight')
     rightPressed = true
 
@@ -62,58 +80,25 @@ function keyDownHandler(event){
     leftPressed = true
 }
 function keyUpHandler(event){
-    console.log("keyup")
     if(event.key == 'Left'|| event.key == 'ArrowLeft')
     leftPressed = false
 
     else if(event.key == 'Right'|| event.key == 'ArrowRight')
     rightPressed = false
 }
+function gameLoop(){
+    //  requestAnimationFrame(gameLoop)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    window.onresize = function(){ 
+    location.reload()
+     }
 
-function drawBall(){
-    
+    circle.draw()
+    circle.update()
+    pallet.draw()
+    pallet.update()
 }
-function drawPallet(){
-    context.beginPath()
-    context.rect(palletX,palletY,pallet_width,pallet_height)
-    context.fillStyle = 'black'
-    context.fill()
-}
-
-circle.update()
-// let fn = 
-// gupdate.bind(circle)
-
-// fn()
-// update()
-// function gameLoop(){
-//      requestAnimationFrame(gameLoop)
-
-//     context.clearRect(0, 0, canvas.width, canvas.height)
-//     drawPallet()
-    
-//     circle.update()
-//     circle.draw()
-   
-//     if(rightPressed){
-//         palletX+= 10
-//         console.log("right")
-//         if (palletX + pallet_width > canvas.width){
-//             palletX = canvas.width - pallet_width
-//         }
-//     }
-//     else if(leftPressed){
-//     palletX+= -10
-//     if(palletX < 0){
-//     palletX = 0
-//     }
-// }
-
-
-// }
-
-
-
+var interval = setInterval(gameLoop, 15);
 gameLoop() //inifinte ierate
 
 
